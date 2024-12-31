3c2f055d93e0a99a7396b9f5d8c91a6a00d4399e
kranthimamatha <lakshmisonkranthi@gmail.com>
2024-12-31 12:03:48 +0530

    added header and home pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Home from './home';
 
 
 
 function App() {
   return (
     <div className="bg-gray-100 h-screen flex justify-center items-center">
+      <Home />
       <h1 className="text-4xl font-bold text-blue-500">Welcome to Healthcare Management System!</h1>
       <button className="bg-primary text-white px-4 py-2 rounded">
         Book Appointment
